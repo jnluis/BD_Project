9c05e8da1e22906c7145b02cb6f4c69d62b162a5
Diana Miranda <dianarrmiranda@gmail.com>
2023-04-05 17:41:18 +0100

    ER

--- DDL/tables.sql
+++ DDL/tables.sql
@@ -1,41 +1,32 @@
-USE Project;
-GO
+--USE Project;
+--GO
 
-ALTER TABLE Ginasio.Cliente DROP CONSTRAINT IF EXISTS ID_Treino;
-ALTER TABLE Ginasio.Treinador DROP CONSTRAINT IF EXISTS Num_Trein;
-ALTER TABLE Ginasio.Rececionista DROP CONSTRAINT IF EXISTS Num_Recec;
-ALTER TABLE Ginasio.Gerente DROP CONSTRAINT IF EXISTS Num_Gere;
-ALTER TABLE Ginasio.Staff DROP CONSTRAINT IF EXISTS Gerente_Num;
-ALTER TABLE Ginasio.Plano_Adesao DROP CONSTRAINT IF EXISTS CC_Cliente;
-ALTER TABLE Ginasio.Plano_Adesao DROP CONSTRAINT IF EXISTS Num_Rec;
-ALTER TABLE Ginasio.Pagamento DROP CONSTRAINT IF EXISTS CC_Cliente_Paga;
-ALTER TABLE Ginasio.Pagamento DROP CONSTRAINT IF EXISTS Num_Rec_Paga;
-ALTER TABLE Ginasio.Feedback DROP CONSTRAINT IF EXISTS CC_Cliente_Feed;
-ALTER TABLE Ginasio.Feedback DROP CONSTRAINT IF EXISTS ID_Treinador_Feed;
-ALTER TABLE Ginasio.Aula DROP CONSTRAINT IF EXISTS Sala_ID;
-ALTER TABLE Ginasio.Horario DROP CONSTRAINT IF EXISTS Aula_ID;
-ALTER TABLE Ginasio.Plano_Treino DROP CONSTRAINT IF EXISTS ID_Treinador_PlanoT;
-ALTER TABLE Ginasio.Plano_Treino DROP CONSTRAINT IF EXISTS CC_Cliente_PlanoT;
-ALTER TABLE Ginasio.Exercicio DROP CONSTRAINT IF EXISTS ID_Equipamento;
-ALTER TABLE Ginasio.Inclui DROP CONSTRAINT IF EXISTS ID_Aula_Insc;
-ALTER TABLE Ginasio.Inclui DROP CONSTRAINT IF EXISTS CC_Cliente_Insc;
+IF OBJECT_ID('Ginasio.Exercicio', 'U') IS NOT NULL
+AND OBJECT_ID('Ginasio.Aula', 'U') IS NOT NULL
+AND OBJECT_ID('Ginasio.Staff', 'U') IS NOT NULL
+BEGIN
+    ALTER TABLE Ginasio.Exercicio DROP CONSTRAINT IF EXISTS ID_Equipamento;
+    ALTER TABLE Ginasio.Aula DROP CONSTRAINT IF EXISTS Sala_ID;
+    ALTER TABLE Ginasio.Staff DROP CONSTRAINT IF EXISTS Gerente_Num;
+END
 
-DROP TABLE IF EXISTS Ginasio.Cliente;
-DROP TABLE IF EXISTS Ginasio.Staff;
-DROP TABLE IF EXISTS Ginasio.Treinador;
-DROP TABLE IF EXISTS Ginasio.Rececionista;
-DROP TABLE IF EXISTS Ginasio.Gerente;
+DROP TABLE IF EXISTS Ginasio.Certificacoes_Prof;
+DROP TABLE IF EXISTS Ginasio.Inscreve;
+DROP TABLE IF EXISTS Ginasio.Inclui;
+DROP TABLE IF EXISTS Ginasio.Equipamento;
+DROP TABLE IF EXISTS Ginasio.Exercicio;
+DROP TABLE IF EXISTS Ginasio.Plano_Treino;
+DROP TABLE IF EXISTS Ginasio.Sala;
+DROP TABLE IF EXISTS Ginasio.Aula_Horario
+DROP TABLE IF EXISTS Ginasio.Aula;
+DROP TABLE IF EXISTS Ginasio.Feedback;
 DROP TABLE IF EXISTS Ginasio.Pagamento;
 DROP TABLE IF EXISTS Ginasio.Plano_Adesao;
-DROP TABLE IF EXISTS Ginasio.Feedback;
-DROP TABLE IF EXISTS Ginasio.Aula;
-DROP TABLE IF EXISTS Ginasio.Horario;
-DROP TABLE IF EXISTS Ginasio.Sala;
-DROP TABLE IF EXISTS Ginasio.Plano_Treino;
-DROP TABLE IF EXISTS Ginasio.Exercicio;
-DROP TABLE IF EXISTS Ginasio.Equipamento;
-DROP TABLE IF EXISTS Ginasio.Inclui;
-DROP TABLE IF EXISTS Ginasio.Inscreve;
+DROP TABLE IF EXISTS Ginasio.Gerente;
+DROP TABLE IF EXISTS Ginasio.Rececionista;
+DROP TABLE IF EXISTS Ginasio.Professor;
+DROP TABLE IF EXISTS Ginasio.Staff;
+DROP TABLE IF EXISTS Ginasio.Cliente;
 
 DROP SCHEMA IF EXISTS Ginasio;
 GO
@@ -51,8 +42,7 @@ CREATE TABLE Ginasio.Cliente(
     Telemovel   INT             NOT NULL    CHECK (Telemovel >= 900000000 AND Telemovel <= 999999999),
     NIF         INT                         CHECK (NIF >= 100000000 AND NIF <= 999999999),
     Morada      VARCHAR(100),
-    Data_Nasc   DATE            NOT NULL,
-    ID_Treino   INT             NOT NULL,
+    Data_Nasc   DATE,
 
     PRIMARY KEY (CC)
 );
@@ -70,28 +60,43 @@ CREATE TABLE Ginasio.Staff(
     Num_func    INT             NOT NULL,   
     Data_Contr  DATE            NOT NULL,
     Horario_Lab TIME            NOT NULL,
-    Gerente_Num INT,
+    Gerente_Num INT             NOT NULL,
 
     PRIMARY KEY (Num_func)
 );
 
-CREATE TABLE Ginasio.Treinador(
+CREATE TABLE Ginasio.Professor(
     Num_func        INT         NOT NULL,
-    Certificacoes   VARCHAR(100), -- de que tipo é isto???
 
-    PRIMARY KEY (Num_func)
+    PRIMARY KEY (Num_func),
+    FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func)
 );
 
 CREATE TABLE Ginasio.Rececionista(
     Num_func    INT             NOT NULL,
 
-    PRIMARY KEY (Num_func)
+    PRIMARY KEY (Num_func),
+    FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func)
 );
 
 CREATE TABLE Ginasio.Gerente(
     Num_func    INT             NOT NULL,
 
-    PRIMARY KEY (Num_func)
+    PRIMARY KEY (Num_func),
+    FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func)
+);
+
+CREATE TABLE Ginasio.Plano_Adesao(
+    Tipo        VARCHAR(50)     NOT NULL,
+    CC_Cliente  INT             NOT NULL,
+    Preco       SMALLMONEY      NOT NULL,
+    Data_fim    DATE            NOT NULL,
+    Data_inicio DATE            NOT NULL, 
+    Num_Rec     INT             NOT NULL,
+
+    PRIMARY KEY (CC_Cliente,Num_Rec),
+    FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC),
+    FOREIGN KEY (Num_Rec) REFERENCES Ginasio.Rececionista(Num_func)
 );
 
 CREATE TABLE Ginasio.Pagamento(
@@ -100,49 +105,46 @@ CREATE TABLE Ginasio.Pagamento(
     Metodo      VARCHAR(50)     NOT NULL,
     Estado      VARCHAR(50)     NOT NULL,
     Data_venc   DATE            NOT NULL,
-    Data_canc   DATE            NOT NULL,
+    Data_canc   DATE,
     CC_Cliente  INT             NOT NULL,
-    Num_Rec     INT             NOT NULL-- de que tipo é isto???
+    Num_Rec     INT             NOT NULL,
+    Data_Pagamento DATE,
 
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CC_Cliente, Num_Rec) REFERENCES Ginasio.Plano_Adesao(CC_Cliente, Num_Rec),
+    FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC)
 );
 
-CREATE TABLE Ginasio.Plano_Adesao(
-    Tipo        VARCHAR(50)     NOT NULL,
-    CC_Cliente  INT             NOT NULL,
-    Preco       SMALLMONEY      NOT NULL,
-    Servicos    VARCHAR(50)     NOT NULL,
-    Data_fim    DATE            NOT NULL,
-    Data_inicio DATE            NOT NULL, 
-    Num_Rec     INT             NOT NULL-- de que tipo é isto???
 
-    PRIMARY KEY (CC_Cliente,Num_Rec)
-);
 
 CREATE TABLE Ginasio.Feedback(
     CC_Cliente      INT             NOT NULL,
-    ID_Treinador    INT             NOT NULL,
+    ID_Professor    INT             NOT NULL,
     Comentários     VARCHAR(800)    NOT NULL,
     [Data]          DATE            NOT NULL,
 
-    PRIMARY KEY (CC_Cliente, ID_Treinador)
+    PRIMARY KEY (CC_Cliente, ID_Professor),
+    FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC),
+    FOREIGN KEY (ID_Professor) REFERENCES Ginasio.Professor(Num_func)
 );
 
 CREATE TABLE Ginasio.Aula(
-    ID          INT             NOT NULL,
-    Sala_ID     INT             NOT NULL,
-    Num_Insc    INT             NOT NULL,    
-    
-    PRIMARY KEY (ID)
+    ID          INT    NOT NULL,
+    Sala_ID     INT,
+    Num_Insc    INT    NOT NULL,    
+    ID_Professor     INT    NOT NULL,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (ID_Professor) REFERENCES Ginasio.Professor(Num_func)
 );
 
-CREATE TABLE Ginasio.Horario(
+CREATE TABLE Ginasio.Aula_Horario(
     Aula_ID     INT             NOT NULL,
     Hora_Inicio TIME            NOT NULL,
     Hora_Fim    TIME            NOT NULL,
     Dia_Semana  VARCHAR(50)     NOT NULL,    
     
-    PRIMARY KEY (Aula_ID)
+    PRIMARY KEY (Aula_ID, Hora_Inicio, Hora_Fim, Dia_Semana),
+    FOREIGN KEY (Aula_ID) REFERENCES Ginasio.Aula(ID)
 );
 
 CREATE TABLE Ginasio.Sala(
@@ -156,18 +158,20 @@ CREATE TABLE Ginasio.Sala(
 CREATE TABLE Ginasio.Plano_Treino(
     ID                      INT     NOT NULL,
     Data_Inicio             DATE    NOT NULL,
-    Data_Fim                DATE    NOT NULL, 
+    Data_Fim                DATE, 
     Num_Treinos_Semanais    TINYINT NOT NULL,
-    ID_Treinador            INT     NOT NULL,
+    ID_Professor            INT     NOT NULL,
     CC_Cliente              INT     NOT NULL,
 
-    PRIMARY KEY (ID)
+    PRIMARY KEY (ID),
+    FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC),
+    FOREIGN KEY (ID_Professor) REFERENCES Ginasio.Professor(Num_func)
 );
 
 CREATE TABLE Ginasio.Exercicio(
-    ID              INT     NOT NULL,
-    Num_Reps        TINYINT NOT NULL,
-    ID_Equipamento  INT     NOT NULL,
+    ID              INT          NOT NULL,
+    Nome            VARCHAR(50) NOT NULL,
+    ID_Equipamento  INT,
  
     PRIMARY KEY (ID)
 );
@@ -182,49 +186,34 @@ CREATE TABLE Ginasio.Equipamento(
 CREATE TABLE Ginasio.Inclui(
     ID_Ex       INT         NOT NULL,
     ID_PT       INT         NOT NULL,
-    Tempo       TIME        NOT NULL,
-    Num_Series  TINYINT     NOT NULL,
-    Nome        VARCHAR(50) NOT NULL,
+    Tempo       TIME,
+    Num_Series  INT,
+    Num_Reps    INT,
  
-    PRIMARY KEY (ID_Ex, ID_PT)
+    PRIMARY KEY (ID_Ex, ID_PT),
+    FOREIGN KEY (ID_Ex) REFERENCES Ginasio.Exercicio(ID),
+    FOREIGN KEY (ID_PT) REFERENCES Ginasio.Plano_Treino(ID)
 );
 
 CREATE TABLE Ginasio.Inscreve(
     ID_Aula     INT         NOT NULL,
     CC_Cliente  INT         NOT NULL,
-    Estado      VARCHAR(50) NOT NULL,
+    Estado      VARCHAR(50),
 
-    PRIMARY KEY (ID_Aula, CC_Cliente)
+    PRIMARY KEY (ID_Aula, CC_Cliente),
+    FOREIGN KEY (ID_Aula) REFERENCES Ginasio.Aula(ID),
+    FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC)
 ); 
 
-ALTER TABLE Ginasio.cliente ADD CONSTRAINT ID_Treino FOREIGN KEY (ID_Treino) REFERENCES Ginasio.Plano_Treino(ID)
+CREATE TABLE Ginasio.Certificacoes_Prof(
+    Num_func      INT         NOT NULL,
+    Certificacoes VARCHAR(100)  NOT NULL,
+    FOREIGN KEY (Num_func) REFERENCES Ginasio.Professor(Num_func)
+);
 
-ALTER TABLE Ginasio.Treinador ADD CONSTRAINT Num_Trein FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func) 
-ALTER TABLE Ginasio.Rececionista ADD CONSTRAINT Num_Recec FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func)
-ALTER TABLE Ginasio.Gerente ADD CONSTRAINT Num_Gere FOREIGN KEY (Num_func) REFERENCES Ginasio.Staff(Num_func)
 
-ALTER TABLE Ginasio.Staff ADD CONSTRAINT Gerente_Num FOREIGN KEY (Gerente_Num) REFERENCES Ginasio.Gerente(Num_func)
+ALTER TABLE Ginasio.Staff ADD CONSTRAINT Gerente_Num FOREIGN KEY (Gerente_Num) REFERENCES Ginasio.Gerente(Num_func);
 
-ALTER TABLE Ginasio.Plano_Adesao ADD CONSTRAINT CC_Cliente FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC)
-ALTER TABLE Ginasio.Plano_Adesao ADD CONSTRAINT Num_Rec FOREIGN KEY (Num_Rec) REFERENCES Ginasio.Rececionista(Num_func)
+ALTER TABLE Ginasio.Aula ADD CONSTRAINT Sala_ID FOREIGN KEY (Sala_ID) REFERENCES Ginasio.Sala(ID);
 
-ALTER TABLE Ginasio.Pagamento ADD CONSTRAINT CC_Cliente_Paga FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Plano_Adesao(CC_Cliente)
-ALTER TABLE Ginasio.Pagamento ADD CONSTRAINT Num_Rec_Paga FOREIGN KEY (Num_Rec) REFERENCES Ginasio.Plano_Adesao(Num_Rec)
-
-ALTER TABLE Ginasio.Feedback ADD CONSTRAINT CC_Cliente_Feed FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Plano_Adesao(Num_Rec)
-ALTER TABLE Ginasio.Feedback ADD CONSTRAINT ID_Treinador_Feed FOREIGN KEY (ID_Treinador) REFERENCES Ginasio.Treinador(Num_func)
-
-ALTER TABLE Ginasio.Aula ADD CONSTRAINT Sala_ID FOREIGN KEY (Sala_ID) REFERENCES Ginasio.Sala(ID)
-
-ALTER TABLE Ginasio.Horario ADD CONSTRAINT Aula_ID FOREIGN KEY (Aula_ID) REFERENCES Ginasio.Aula(ID)
-
-ALTER TABLE Ginasio.Plano_Treino ADD CONSTRAINT ID_Treinador_PlanoT FOREIGN KEY (ID_Treinador) REFERENCES Ginasio.Treinador(Num_func)
-ALTER TABLE Ginasio.Plano_Treino ADD CONSTRAINT CC_Cliente_PlanoT FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC)
-
-ALTER TABLE Ginasio.Exercicio ADD CONSTRAINT ID_Equipamento FOREIGN KEY (ID_Equipamento) REFERENCES Ginasio.Equipamento(ID)
-
-ALTER TABLE Ginasio.Inclui ADD CONSTRAINT ID_Ex FOREIGN KEY (ID_Ex) REFERENCES Ginasio.Exercicio(ID)
-ALTER TABLE Ginasio.Inclui ADD CONSTRAINT ID_PT FOREIGN KEY (ID_PT) REFERENCES Ginasio.Plano_Treino(ID)
-
-ALTER TABLE Ginasio.Inscreve ADD CONSTRAINT ID_Aula_Insc FOREIGN KEY (ID_Aula) REFERENCES Ginasio.Horario(Aula_ID) -- aqui temos horario(aula_ID) Mas como essa é referenciada  à AUla(ID) não deveria ser Aula(ID), não podemos fazer esta ligação direta
-ALTER TABLE Ginasio.Inscreve ADD CONSTRAINT CC_Cliente_Insc FOREIGN KEY (CC_Cliente) REFERENCES Ginasio.Cliente(CC)
+ALTER TABLE Ginasio.Exercicio ADD CONSTRAINT ID_Equipamento FOREIGN KEY (ID_Equipamento) REFERENCES Ginasio.Equipamento(ID);
--- apfe_submit.md
+++ apfe_submit.md
@@ -10,6 +10,22 @@ Escreva uma pequena introdução sobre o trabalho.
 
 ## ​Análise de Requisitos / Requirements
 
+* Um cliente é caracterizado por nome, CC, data de nascimento, morada, contribuinte, número telemóvel e email.
+* Quando um cliente se inscreve pela primeira vez no ginásio, é necessário subscrever um plano de adesão que pode ser mensal, anual ou semanal.
+* Um plano de adesão é definido por um tipo de plano, preço, data de início, data de fim.
+* Quando um plano de adesão é criado pelo Rececionista, é emitido um pagamento para o cliente realizar, que aquando realizado é registado a data. Este pagamento é caracterizado por um ID, valor, método, estado, data de vencimento e data de cancelamento (se se aplicar).
+* Todos os membros do staff são identificados por número de funcionário, nome, CC, NIF, data de nascimento, e-mail, número de telemóvel, morada, horário de trabalho, salário e data de contratação.
+* O cliente tem a opção de solicitar um plano de treino a um professor. Esse plano é definido por um ID, data de início, data de fim, número de treinos por semana. Para cada exercício, é necessário especificar o ID, nome e quando este é incluído num plano é possível definir o número de séries, número de repetições e tempo de execução. O exercício pode utilizar ou não um equipamento.
+* O equipamento é caracterizado por ID e nome.
+* O professor faz parte do staff, no entanto tem também de entregar as suas certificações.
+* O professor pode dar aulas no ginásio, que são identificadas por um ID. É registado em cada aula o número de inscrições e a sala onde se vai realizar. Cada aula pode ser realizada em vários horários diferentes. Cada horário é definido pela hora de início e fim e pelo dia da semana em que se vai realizar.
+* Cada sala é definida por um ID, número máximo de alunos e estado (ocupado ou livre).
+* O cliente pode inscrever-se em várias aulas desde que haja disponibilidade e a inscrição seja aceite.
+* O cliente tem a opção de fornecer feedback sobre o seu plano de treino ao treinador que o criou. Este feedback é caracterizado pela data e comentários associados.
+* Embora seja um membro do staff, o gerente tem a responsabilidade de gerir a equipa de funcionários.
+
+
+
 
 ## DER
 
