8d542e61809746e795fa543c43ce9449e23ecabe
Diana Miranda <dianarrmiranda@gmail.com>
2023-05-17 22:14:16 +0100

    atualizei os dados da tabela inclui

--- UDFFF.sql
+++ UDFFF.sql
@@ -4,9 +4,9 @@ TABLE([nameEx] VARCHAR(50), [numReps] int, [numSeries] int, [tempo] time)
 AS
     BEGIN
         INSERT @table
-        SELECT  Company.Project.Pname, Company.Project.Plocation FROM Company.Project
-        INNER JOIN Company.Works_on ON Company.Works_on.Pno = Company.Project.Pnumber
-        WHERE Works_on.Essn=@func_ssn
+		SELECT Ginasio.Exercicio.Nome,Ginasio.Inclui.Tempo, Ginasio.Inclui.Num_Reps, Ginasio.Inclui.Num_Series 
+		From  Ginasio.Cliente join Ginasio.Plano_Treino on Ginasio.Cliente.CC = Ginasio.Plano_Treino.CC_Cliente join Ginasio.Inclui on Ginasio.Inclui.ID_PT = Ginasio.Plano_Treino.ID join Ginasio.Exercicio on Ginasio.Exercicio.ID = Ginasio.Inclui.ID_Ex
+        WHERE Ginasio.Cliente.CC= @CC_Cliente
 		RETURN;
     END
 GO
--- UDFPlanoTreino.sql
+++ UDFPlanoTreino.sql
@@ -1,3 +1,3 @@
 SELECT Ginasio.Exercicio.Nome,Ginasio.Inclui.Tempo, Ginasio.Inclui.Num_Reps, Ginasio.Inclui.Num_Series 
-From  Ginasio.Cliente join Ginasio.Plano_Treino on Ginasio.Cliente.CC = Ginasio.Plano_Treino.CC_Cliente
-WHERE Works_on.Essn=@func_ssn
+From  Ginasio.Cliente join Ginasio.Plano_Treino on Ginasio.Cliente.CC = Ginasio.Plano_Treino.CC_Cliente join Ginasio.Inclui on Ginasio.Inclui.ID_PT = Ginasio.Plano_Treino.ID join Ginasio.Exercicio on Ginasio.Exercicio.ID = Ginasio.Inclui.ID_Ex
+WHERE Ginasio.Cliente.CC = '123456789'
